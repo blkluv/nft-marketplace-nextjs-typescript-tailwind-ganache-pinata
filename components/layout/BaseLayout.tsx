6a5a1af325ad5bce2859363852f706225e512694
Michael Mburu <ichangaimichael@yahoo.com>
2023-02-12 00:06:46 +0300

    NFT Marketplace NextJs Setup
    Added TailwindUI Header

---
+++ components/layout/BaseLayout.tsx
@@ -0,0 +1,18 @@
+import { FunctionComponent, PropsWithChildren } from 'react'
+import { Navbar } from '..'
+
+const BaseLayout: FunctionComponent<PropsWithChildren> = ({ children }) => {
+  return (
+    <>
+    <Navbar />
+    <div className='py-16 bg-gray-50 overflow-hidden min-h-screen'>
+      <div className='max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8'>
+        {children}
+      </div>
+    </div>
+    </>
+  
+  )
+}
+
+export default BaseLayout
