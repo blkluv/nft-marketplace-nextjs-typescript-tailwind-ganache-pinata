bfde1bcbd7720e9b2fa6a52e1e9c970703086f5f
Michael Mburu <ichangaimichael@yahoo.com>
2023-02-12 15:41:06 +0300

    Added NFT Components

---
+++ components/nft/list/index.tsx
@@ -0,0 +1,23 @@
+import { FunctionComponent, PropsWithChildren } from 'react'
+import NFTItem from '../item'
+import { NFTMetaData } from '../../../types/nft'
+type NFTListProps = {
+  nfts: NFTMetaData[]
+}
+const NFTList: FunctionComponent<NFTListProps> = ({ nfts }) => {
+  return (
+    <div className='mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none'>
+      {nfts &&
+        nfts.map((nft) => (
+          <div
+            key={nft.image}
+            className='flex flex-col rounded-lg shadow-lg overflow-hidden'
+          >
+            <NFTItem item={nft} />
+          </div>
+        ))}
+    </div>
+  )
+}
+
+export default NFTList
---
+++ types/nft.ts
@@ -0,0 +1,15 @@
+import { type } from 'os'
+
+export type Trait = 'attack' | 'health' | 'speed'
+
+export type NFTAttribute = {
+  trait_type: Trait
+  value: string
+}
+
+export type NFTMetaData = {
+  name: string
+  description: string
+  image: string
+  attributes: NFTAttribute[]
+}
